7e215a7d5ec34e7cbd4ad70e2d143325926b056f
yuchen <yuchenz27@outlook.com>
2023-02-01 16:06:09 +0800

    Added RttCounter

---
+++ Assets/Scripts/RttCounter.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class RttCounter : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log($"[RttCounter] OnNetworkSpawn");
+    }
+
+    private void Update()
+    {
+        if (IsSpawned && !IsServer)
+        {
+            RequestRttServerRpc(Time.time);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestRttServerRpc(float clientTimestamp, ServerRpcParams serverRpcParams = default)
+    {
+        // NOTE! In case you know a list of ClientId's ahead of time, that does not need change,
+        // Then please consider caching this (as a member variable), to avoid Allocating Memory every time you run this function
+        ClientRpcParams clientRpcParams = new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams
+            {
+                TargetClientIds = new ulong[] { serverRpcParams.Receive.SenderClientId }
+            }
+        };
+        RespondRttClientRpc(clientTimestamp, clientRpcParams);
+    }
+
+    [ClientRpc]
+    private void RespondRttClientRpc(float clientTimestamp, ClientRpcParams _ = default)
+    {
+        float rtt = (Time.time - clientTimestamp) * 1000f;
+        Debug.Log($"[RttCounter] Rtt is {rtt} ms");
+    }
+}
