7ece9592a1e182b7753cb57442049a0a6acf4f50
yuchen <yuchenz27@outlook.com>
2023-02-04 18:47:13 +0800

    added configuration settings on UI

--- Assets/Scripts/UI/StartPanel.cs
+++ Assets/Scripts/UI/StartPanel.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Netcode.Transports.MultipeerConnectivity;
 
@@ -7,8 +5,36 @@ public class StartPanel : MonoBehaviour
 {
     public void OnNicknameChanged(string nickname)
     {
-        Debug.Log($"[StartPanel] OnNicknameChanged {nickname}");
-
         MultipeerConnectivityTransport.Instance.Nickname = nickname;
+
+        Debug.Log($"Nickname changed to {nickname}");
+    }
+
+    public void OnAutoAdvertiseToggled(bool value)
+    {
+        MultipeerConnectivityTransport.Instance.AutoAdvertise = value;
+
+        Debug.Log($"AutoAdvertise changed to {value}");
+    }
+
+    public void OnAutoApproveConnectionRequestToggled(bool value)
+    {
+        MultipeerConnectivityTransport.Instance.AutoApproveConnectionRequest = value;
+
+        Debug.Log($"AutoApproveConnectionRequest changed to {value}");
+    }
+
+    public void OnAutoBrowseToggled(bool value)
+    {
+        MultipeerConnectivityTransport.Instance.AutoBrowse = value;
+
+        Debug.Log($"AutoBrowse changed to {value}");
+    }
+
+    public void OnAutoSendConnectionRequestToggled(bool value)
+    {
+        MultipeerConnectivityTransport.Instance.AutoSendConnectionRequest = value;
+
+        Debug.Log($"AutoSendConnectionRequest changed to {value}");
     }
 }
